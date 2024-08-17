9b360fd364a78e3a41b5249f531cc289b57baec8
Sud <155895839+Sud-7@users.noreply.github.com>
2024-08-17 19:47:15 +0530

    Image added and changed next.config file

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,4 +1,6 @@
 import Product22 from "./product";
+import image from "../../../public/vercel.svg";
+import Image from "next/image";
 
 async function productlist() {
   let data = await fetch("https://dummyjson.com/products");
@@ -12,6 +14,12 @@ export default async function Product() {
   return (
     <div>
       <h1>Product List</h1>
+      <Image src={image} height={200} width={200} />
+      <Image
+        src="https://images.freeimages.com/image/previews/477/sweet-flat-candy-png-5690113.png"
+        width={200}
+        height={200}
+      />
       {product.map((item) => (
         <div>
           <h3 key={item.id}>Name: {item.title}</h3>
